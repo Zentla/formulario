18429d1f1e0157814dc77af6a6dc2d8a09d43f88
Zentla <51496839+Zentla@users.noreply.github.com>
2021-05-08 00:37:09 -0500

    segund

--- correo.php
+++ correo.php
@@ -1,20 +1,16 @@
 <?php
-if (isset($_POST['enviar'])) {
-    if (!empty($_POST['nombre']) && !empty($_POST['correo']) && !empty($_POST['asunto']) && !empty($_POST['mensaje'])) {
+        $destino = 'fernandohernandezortela@gmail.com';
         $nombre = $_POST['nombre'];
         $correo = $_POST['correo'];
         $asunto = $_POST['asunto'];
         $mensaje = $_POST['mensaje'];
-        $header = "From: noreply@example.com" . "\r\n";
-        $header = "Reply-To: noreply@example.com" . "\r\n";
-        $header = "X-Mailer: PHP/" . phpversion();
-        $mail = @mail($correo, $asunto, $mensaje, $header);
-        if ($mail){
-            echo "<h4>¡Mail enviado exitosamente!</h4>";
-        }
 
-    }
-} 
+        $header = "Enviado desde un lugar";
+        $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+        mail($destino, asunto, $mensajeCompleto, $header);
 
+        echo "<script>alert('Exitoso')</script>";
+        echo "<script>setTimeout("\location.href='index.html'\", 1000)</script>";
+?>
 
 
